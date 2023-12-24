b6bda046de26738900d147ddfd46896876fca60d
Ryan Croy <ryanc119@gmail.com>
2023-12-24 07:10:07 -0600

    Add pseudocode to javascript fle

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. Ask the user to provide a number
+// 2. For each number up to the number the user has provided
+// 3. If the number is only divisible by 3, print _Fizz_ to the console
+// 4. If the number is only divisible by 5, print _Buzz_ to the console
+// 5. If the number is divisible by both 3 and 5, print _FizzBuzz_ to the console
+// 6. Otherwise, just print the number to the console
